c9fc96d7b8561ef161c9a92461cca9456abffe24
Rangel Ivanov <rangelivanov88@gmail.com>
2013-01-10 18:16:38 +0200

    Propagate checked state change to child views

--- library/src/com/mobeta/android/dslv/DragSortItemView.java
+++ library/src/com/mobeta/android/dslv/DragSortItemView.java
@@ -1,31 +1,26 @@
+
 package com.mobeta.android.dslv;
 
 import android.content.Context;
 import android.view.Gravity;
 import android.view.View;
-import android.view.View.MeasureSpec;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
-import android.util.Log;
+import android.widget.Checkable;
 
 /**
- * Lightweight ViewGroup that wraps list items obtained from user's
- * ListAdapter. ItemView expects a single child that has a definite
- * height (i.e. the child's layout height is not MATCH_PARENT).
- * The width of
- * ItemView will always match the width of its child (that is,
- * the width MeasureSpec given to ItemView is passed directly
- * to the child, and the ItemView measured width is set to the
- * child's measured width). The height of ItemView can be anything;
- * the 
- * 
- *
- * The purpose of this class is to optimize slide
- * shuffle animations.
+ * Lightweight ViewGroup that wraps list items obtained from user's ListAdapter.
+ * ItemView expects a single child that has a definite height (i.e. the child's
+ * layout height is not MATCH_PARENT). The width of ItemView will always match
+ * the width of its child (that is, the width MeasureSpec given to ItemView is
+ * passed directly to the child, and the ItemView measured width is set to the
+ * child's measured width). The height of ItemView can be anything; the The
+ * purpose of this class is to optimize slide shuffle animations.
  */
-public class DragSortItemView extends ViewGroup {
+public class DragSortItemView extends ViewGroup implements Checkable {
 
     private int mGravity = Gravity.TOP;
+    private boolean checked;
 
     public DragSortItemView(Context context) {
         super(context);
@@ -57,7 +52,8 @@ public class DragSortItemView extends ViewGroup {
         if (mGravity == Gravity.TOP) {
             child.layout(0, 0, getMeasuredWidth(), child.getMeasuredHeight());
         } else {
-            child.layout(0, getMeasuredHeight() - child.getMeasuredHeight(), getMeasuredWidth(), getMeasuredHeight());
+            child.layout(0, getMeasuredHeight() - child.getMeasuredHeight(), getMeasuredWidth(),
+                    getMeasuredHeight());
         }
     }
 
@@ -66,7 +62,7 @@ public class DragSortItemView extends ViewGroup {
      */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        
+
         int height = MeasureSpec.getSize(heightMeasureSpec);
         int width = MeasureSpec.getSize(widthMeasureSpec);
 
@@ -95,6 +91,29 @@ public class DragSortItemView extends ViewGroup {
         }
 
         setMeasuredDimension(width, height);
+    }
+
+    @Override
+    public boolean isChecked() {
+        View child = getChildAt(0);
+        if (child instanceof Checkable)
+            return ((Checkable) child).isChecked();
+        else
+            return false;
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        View child = getChildAt(0);
+        if (child instanceof Checkable)
+            ((Checkable) child).setChecked(checked);
+    }
+
+    @Override
+    public void toggle() {
+        View child = getChildAt(0);
+        if (child instanceof Checkable)
+            ((Checkable) child).toggle();
     }
 
 }
